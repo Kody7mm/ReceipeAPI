c1e06e0e3743efb0a5940ea4b8192efaed7d10d8
James Wyn <thisisjameswyn@gmail.com>
2021-02-10 15:20:06 -0800

    remove sensitve data
    Co-authored-by: Daniel Adeyemi <adeyemidany@gmail.com>

--- src/receipe.js
+++ src/receipe.js
@@ -1,6 +1,6 @@
 export default class Receipe {
   static getReceipe() {
-    return fetch(`https://api.nasa.gov/planetary/apod?api_key=sQh5wBQUo0IbcvJxmljNFbmx1qd6dfRXLoBrJbjW`)
+    return fetch(`https://api.nasa.gov/planetary/apod?api_key=`)
       .then(function(response) {
         if (!response.ok) {
           throw Error(response.statusText);
